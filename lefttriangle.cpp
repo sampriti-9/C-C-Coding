97e6a5746ad52a79ecf77abd874f2b364403405d
Sampriti <Sampriti@Laptop.com>
2025-09-14 10:48:05 +0530

    patter

---
+++ lefttriangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void leftTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n"; // ye inner loop ke baad hona chahiye
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    leftTriangle(n);
+    return 0;
+}
